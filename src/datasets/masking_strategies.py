2d327b870c089b6753e8b5367a43c9a3b7281d2d
Carolina <carolmou@mit.edu>
2023-08-02 14:36:54 -0300

    Fix empty SEP call in masking_strategies

--- src/datasets/masking_strategies.py
+++ src/datasets/masking_strategies.py
@@ -1,6 +1,7 @@
 import torch
 
-def mask_random_positions(tensor, k = 0.15):
+
+def mask_random_positions(tensor, k=0.15):
     """
     Receives a tensor and returns
     a new tensor of the same shape randomly filled
@@ -8,27 +9,31 @@ def mask_random_positions(tensor, k = 0.15):
     """
     shape = tensor.shape
     rand = torch.rand(shape)
-    return (rand < k)
+    return rand < k
+
 
 def mask_end_positions(tensor_ids):
-  """
-  Receives a tensor and returns
-  a tensor of the same shape filled with False's
-  and a single True, at the end.
-  """
-  shape = tensor_ids.shape
-  masks = torch.zeros(shape) != 0
-
-  for i in range(0, shape[0]):
-    for j in range(0, masks[i].shape[0]):
-      if tensor_ids[i][j] == SEP:
-        masks[i][j-1] = True
-  return masks
-
-  def mask_nothing(tensor):
     """
-    Receives a tensor and returns a tensor
-    of the same shape filled with False's.
+    Receives a tensor and returns
+    a tensor of the same shape filled with False's
+    and a single True, at the end.
     """
-    shape = tensor.shape
-    return torch.full(shape, False)
+    shape = tensor_ids.shape
+    masks = torch.zeros(shape) != 0
+
+    # this is not SFB, please fix it
+    SEP = 102
+
+    for i in range(0, shape[0]):
+        for j in range(0, masks[i].shape[0]):
+            if tensor_ids[i][j] == SEP:
+                masks[i][j - 1] = True
+    return masks
+
+def mask_nothing(tensor):
+  """
+  Receives a tensor and returns a tensor
+  of the same shape filled with False's.
+  """
+  shape = tensor.shape
+  return torch.full(shape, False)
